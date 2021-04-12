e6d3b3e6bdba9026ada9117c20b68ab18d869017
charley <charles.lemaire.1@ens.etsmtl.ca>
2021-04-12 11:42:13 -0400

    first commit

---
+++ Assets/Scripts/Detection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detection : MonoBehaviour
+{
+
+    Color startColor;
+    Color detectedColor;
+    SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        startColor = spriteRenderer.color;
+        detectedColor = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log($"{transform.name} colliding with {collision.transform.name}");
+        spriteRenderer.color = detectedColor;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log($"{transform.name} stopped colliding with {collision.transform.name}");
+        spriteRenderer.color = startColor;
+    }
+}
